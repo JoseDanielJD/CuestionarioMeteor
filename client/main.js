04cddfcecb20775eb348e72edf4447b664c0eacc
JB28 <143jelen@gmail.com>
2017-07-22 17:50:33 +0000

    add items in progress

--- client/main.js
+++ client/main.js
@@ -10,3 +10,32 @@ Template.bar.events({
   }
 });
 
+Template.AddItemTemplate.events({
+     'click .add-more': function(e) { //add new posible answer
+        var dv1,div2,div3,input,button;
+        
+        div1=document.createElement('div');
+        div2=document.createElement('div');
+        div3=document.createElement('div');
+        
+        div1.className="copy-fields";
+        div2.className="control-group input-group";
+        div3.className="input-group-btn";
+        
+        div2.innerHTML='<input type="text" name="addmore[]" class="form-control" placeholder="Type a posible answer">';
+        div3.innerHTML='<button class="btn btn-danger remove" type="button"><i class="glyphicon glyphicon-remove"></i></button>"';
+        
+        div2.appendChild(div3);
+        div1.appendChild(div2);
+        
+        var html = div1;
+        $(".after-add-more").after(html);        
+     },
+ 
+     'click .remove': function(e) { //delete the posible answer
+        console.log("this:",this);
+         $(this).parents(".control-group").remove();
+     }
+ 
+});
+
